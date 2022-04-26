bcb49c45bf6ee94093f91f071e16b75b57e847c9
AlexMomot-717 <momot-717@yandex.ru>
2022-04-26 19:41:43 +0300

    Initial commit

---
+++ todo/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from mainapp.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+
+]
